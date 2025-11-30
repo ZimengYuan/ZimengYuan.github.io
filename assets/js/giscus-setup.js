7abeb153a96ba50aa07ccd5f4163ea88193c6287
ZimengYuan <50367818+ZimengYuan@users.noreply.github.com>
2025-11-30 14:38:44 +0000

    Deploying to gh-pages from @ ZimengYuan/ZimengYuan@fcd6040ef8c5ff23fc7f8f7e76532d9a1a63bcc9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "formoree/ZimengYuan",
+    "data-repo": "ZimengYuan/ZimengYuan",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
